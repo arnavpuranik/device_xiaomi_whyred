81e4ab64a5f830a596e83d23a37812739ac59dac
Arnav Puranik <puranikarnav@gmail.com>
2021-09-08 23:35:13 +0530

    whyred: Update Fingerprint to Redfin September

--- radiant_whyred.mk
+++ radiant_whyred.mk
@@ -41,10 +41,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="whyred" \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210905.001.A1/7511028:user/release-keys
 
 TARGET_VENDOR := Xiaomi
 IS_PHONE := true
